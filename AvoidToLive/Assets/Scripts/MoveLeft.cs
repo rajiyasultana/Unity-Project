6cc86b8247879bdb4ff3db38d56ce1bee39404d3
Rajiya Sultana <rssumu04@gmail.com>
2023-12-08 10:55:51 +0600

    Animation has been added.
    Run, jump and death animation.

--- AvoidToLive/Assets/Scripts/MoveLeft.cs
+++ AvoidToLive/Assets/Scripts/MoveLeft.cs
@@ -2,19 +2,22 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    private float speed = 10;
+    private float speed = 15;
     private float leftBound = -10;
-    
+    private PlayerController playerControllerScript;
 
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left *Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
         if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
--- AvoidToLive/Assets/Scripts/PlayerController.cs
+++ AvoidToLive/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnimation;
     public float jumpforce;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -10,16 +11,18 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnimation = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && gameOver == false)
         {
             playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnimation.SetTrigger("Jump_trig");
         }
     }
 
@@ -33,6 +36,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over!!!!");
             gameOver = true;
+            playerAnimation.SetBool("Death_b", true);
+            playerAnimation.SetInteger("DeathType_int", 1);
         }
         
     }
